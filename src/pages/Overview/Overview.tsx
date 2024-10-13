bada8aff271e298d2076171908ecacc68c25f6bb
Raf <130236858+raf-c@users.noreply.github.com>
2024-10-13 18:56:43 -0400

    Create a test card

--- src/pages/Overview/Overview.tsx
+++ src/pages/Overview/Overview.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { Chart } from '@/components/Chart';
 import { Typography } from '@/components/Typography';
 import { useMe } from '@/hooks/user/useMe';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 
 import styles from './Overview.module.scss';
 
@@ -15,6 +16,15 @@ export const Overview: React.FC = () => {
         Hello Mr {user?.firstName}
       </Typography>
       <Chart width={1448} height={600} />
+      <Card>
+        <CardHeader>
+          <CardTitle>Assets</CardTitle>
+        </CardHeader>
+        <CardContent>$158,960.00</CardContent>
+        <CardFooter>
+          <CardDescription>5.08% higher than last year</CardDescription>
+        </CardFooter>
+      </Card>
     </div>
   );
 };
