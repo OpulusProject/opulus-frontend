f828532799e665a3db671eb41191873a5cb95117
Safewaan <safwaanchowdhury26@gmail.com>
2024-10-23 22:07:08 -0400

    plaid link button

--- src/pages/Overview/Overview.tsx
+++ src/pages/Overview/Overview.tsx
@@ -1,20 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { Chart } from '@/components/Chart';
 import { Typography } from '@/components/Typography';
+import { useLinkToken } from '@/hooks/plaid/useLinkToken';
 import { useMe } from '@/hooks/user/useMe';
 
 import styles from './Overview.module.scss';
+import PlaidLinkButton from './PlaidLinkButton';
 
 export const Overview: React.FC = () => {
   const { data: user } = useMe();
+  const { mutate: createLinkToken, isSuccess } = useLinkToken();
+
+  const [linkToken, setLinkToken] = useState('');
+
+  useEffect(() => {
+    if (user) {
+      createLinkToken(
+        { userID: user.id.toString() },
+        {
+          onSuccess: (data) => {
+            setLinkToken(data.data.link_token);
+          },
+        }
+      );
+    }
+  }, [user, createLinkToken]);
 
   return (
     <div>
       <Typography variant="extra-extra-large">
         Hello Mr {user?.firstName}
       </Typography>
-      <Chart width={1448} height={600} />
+      {/* <Chart width={1448} height={600} /> */}
+      {isSuccess && linkToken && <PlaidLinkButton token={linkToken} />}
     </div>
   );
 };
