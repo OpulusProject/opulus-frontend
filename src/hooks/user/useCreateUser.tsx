adf96a805a634b41874e11a5c31d2fcbd53e4b05
Safewaan <safwaanchowdhury26@gmail.com>
2024-10-24 19:52:39 -0400

    create createUser hook

---
+++ src/hooks/user/useCreateUser.tsx
@@ -0,0 +1,23 @@
+import { useMutation } from '@tanstack/react-query';
+import axios from 'axios';
+
+import { serverURL } from '@/hooks/apiConfig';
+
+const API_URL = `${serverURL}/users`;
+
+interface CreateUserRequest {
+  email: string;
+  password: string;
+}
+
+const createUser = async (request: CreateUserRequest) => {
+  await axios.post(API_URL, request, {
+    withCredentials: true,
+  });
+};
+
+export const useCreateUser = () => {
+  return useMutation({
+    mutationFn: (request: CreateUserRequest) => createUser(request),
+  });
+};
