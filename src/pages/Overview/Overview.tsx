63da02eabf62d95eba325c585ca7b7f5c94c18ed
Raf <130236858+raf-c@users.noreply.github.com>
2024-10-27 16:22:43 -0400

    Create 3 overview cards according to design (for now)

--- src/pages/Overview/Overview.tsx
+++ src/pages/Overview/Overview.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 
+import { OverviewCard } from '@/components/OverviewCard';
 import { Chart } from '@/components/Chart';
 import { Typography } from '@/components/Typography';
 import { useLinkToken } from '@/hooks/plaid/useLinkToken';
@@ -32,6 +33,11 @@ export const Overview: React.FC = () => {
       <Typography variant="extra-extra-large">
         Hello Mr {user?.firstName}
       </Typography>
+      <div className={styles.OverviewCardContainer}>
+        <OverviewCard assetClass={'Net Worth'} amount={185000}></OverviewCard>
+        <OverviewCard assetClass={'November Earnings'} amount={8500}></OverviewCard>
+        <OverviewCard assetClass={'November Spend'} amount={2500}></OverviewCard>
+      </div>
       {/* <Chart width={1448} height={600} /> */}
       {isSuccess && linkToken && <PlaidLinkButton token={linkToken} />}
     </div>
