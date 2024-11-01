bd4faae7a20beeef6b20a32f76c161aa33ae1b6a
Safwaan Chowdhury <57375371+Safewaan@users.noreply.github.com>
2024-11-01 17:56:05 -0400

    remove protected route logic (#13)

--- src/layouts/Auth/Auth.tsx
+++ src/layouts/Auth/Auth.tsx
@@ -6,14 +6,6 @@ import { Cover } from '@/components/Cover';
 import styles from './Auth.module.scss';
 
 export const Auth: React.FC = () => {
-  // some weird stuff with staying in Auth layout while onboarding, since user needs to be logged in to input their first and last name and link their bank account
-  // const { data: user, isSuccess } = useMe();
-  // const isLoggedIn = isSuccess && user !== undefined;
-
-  // if (isLoggedIn) {
-  //   return <Navigate to={ROUTES.HOME} />;
-  // }
-
   return (
     <div className={styles.authLayout}>
       <Cover />
--- src/layouts/Dashboard/Dashboard.tsx
+++ src/layouts/Dashboard/Dashboard.tsx
@@ -1,21 +1,12 @@
 import React from 'react';
-import { Navigate, Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
 import { Navrail } from '@/components/Navrail';
 import { SidebarProvider } from '@/components/ui/sidebar';
-import { useMe } from '@/hooks/user/useMe';
-import { ROUTES } from '@/pages/routes';
 
 import styles from './Dashboard.module.scss';
 
 export const Dashboard: React.FC = () => {
-  const { data: user, isSuccess } = useMe();
-  const isLoggedIn = isSuccess && user !== undefined;
-
-  if (!isLoggedIn) {
-    return <Navigate to={ROUTES.LOGIN} />;
-  }
-
   return (
     <div className={styles.dashboardLayout}>
       <SidebarProvider>
