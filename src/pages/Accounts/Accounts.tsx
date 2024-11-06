fced117a4dd29d24bd81ee9c1357e2271adc798f
Raf <130236858+raf-c@users.noreply.github.com>
2024-11-06 16:31:40 -0500

    Accounts tab

--- src/pages/Accounts/Accounts.tsx
+++ src/pages/Accounts/Accounts.tsx
@@ -2,6 +2,27 @@ import React from 'react';
 
 import { Typography } from '@/components/Typography';
 
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+
+import styles from './Accounts.module.scss';
+
+
 export const Accounts: React.FC = () => {
-  return <Typography variant="extra-extra-large">Accounts</Typography>;
+  return (
+  <>
+  <Typography variant="extra-extra-large">Accounts</Typography>
+
+  <Tabs defaultValue="all" className={styles.tab}>
+  <TabsList>
+    <TabsTrigger value="all">All</TabsTrigger>
+    <TabsTrigger value="cashandsavings">Cash and Savings</TabsTrigger>
+    <TabsTrigger value="stocks">Stocks</TabsTrigger>
+    <TabsTrigger value="Loans">Loans</TabsTrigger>
+    <TabsTrigger value="creditcards">Credit Cards</TabsTrigger>
+  </TabsList>
+</Tabs>
+
+  </>
+
+  );
 };
