a2c9112f6beaa01b976a61e28c1fd8eb9d725dcb
Raf <130236858+raf-c@users.noreply.github.com>
2024-11-09 12:23:14 -0500

    Add account button

--- src/pages/Accounts/Accounts.tsx
+++ src/pages/Accounts/Accounts.tsx
@@ -4,11 +4,20 @@ import { Typography } from '@/components/Typography';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 
 import styles from './Accounts.module.scss';
+import { Button } from '@/components/ui/button';
+
+import { Plus } from 'lucide-react';
 
 export const Accounts: React.FC = () => {
   return (
     <>
-      <Typography variant="extra-extra-large">Accounts</Typography>
+      <div className={styles.header}>
+        <Typography variant="extra-extra-large">Accounts</Typography>
+        <Button className={styles.button}>
+          <Plus size={16}/>
+          Add account
+        </Button>
+      </div>
 
       <Tabs defaultValue="all" className={styles.tab}>
         <TabsList>
