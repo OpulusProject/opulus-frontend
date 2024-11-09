61b85360a36608f43fd10518119882d9f405a574
Raf <130236858+raf-c@users.noreply.github.com>
2024-11-09 12:05:53 -0500

    Format

--- src/pages/Accounts/Accounts.tsx
+++ src/pages/Accounts/Accounts.tsx
@@ -1,28 +1,24 @@
 import React from 'react';
 
 import { Typography } from '@/components/Typography';
-
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 
 import styles from './Accounts.module.scss';
 
-
 export const Accounts: React.FC = () => {
   return (
-  <>
-  <Typography variant="extra-extra-large">Accounts</Typography>
+    <>
+      <Typography variant="extra-extra-large">Accounts</Typography>
 
-  <Tabs defaultValue="all" className={styles.tab}>
-  <TabsList>
-    <TabsTrigger value="all">All</TabsTrigger>
-    <TabsTrigger value="cashandsavings">Cash and Savings</TabsTrigger>
-    <TabsTrigger value="stocks">Stocks</TabsTrigger>
-    <TabsTrigger value="Loans">Loans</TabsTrigger>
-    <TabsTrigger value="creditcards">Credit Cards</TabsTrigger>
-  </TabsList>
-</Tabs>
-
-  </>
-
+      <Tabs defaultValue="all" className={styles.tab}>
+        <TabsList>
+          <TabsTrigger value="all">All</TabsTrigger>
+          <TabsTrigger value="cashandsavings">Cash and Savings</TabsTrigger>
+          <TabsTrigger value="stocks">Stocks</TabsTrigger>
+          <TabsTrigger value="Loans">Loans</TabsTrigger>
+          <TabsTrigger value="creditcards">Credit Cards</TabsTrigger>
+        </TabsList>
+      </Tabs>
+    </>
   );
 };
