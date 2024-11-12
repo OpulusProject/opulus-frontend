c793a529cfcb16973a07f2030a34717ddaaea949
Safewaan <safwaanchowdhury26@gmail.com>
2024-11-11 20:45:38 -0500

    remove test code

--- src/layouts/Dashboard/Dashboard.tsx
+++ src/layouts/Dashboard/Dashboard.tsx
@@ -3,12 +3,10 @@ import { Outlet } from 'react-router-dom';
 
 import { Navrail } from '@/components/Navrail';
 import { SidebarProvider } from '@/components/ui/sidebar';
-import { useMe } from '@/hooks/user/useMe';
 
 import styles from './Dashboard.module.scss';
 
 export const Dashboard: React.FC = () => {
-  const { data: user } = useMe();
   return (
     <div className={styles.dashboardLayout}>
       <SidebarProvider>
