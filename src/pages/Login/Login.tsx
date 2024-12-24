2db7939ec9e41b581143e056c1c17c1f15b3d448
Safewaan <safwaanchowdhury26@gmail.com>
2024-12-24 12:20:58 -0500

    x

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -4,7 +4,7 @@ import { useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
 import { z } from 'zod';
 
-import { GoogleOAuthButton } from '@/components/GoogleOAuthButton/GoogleOAuthButton';
+import { GoogleOAuthButton } from '@/components/GoogleOAuthButton';
 import { Typography } from '@/components/Typography';
 import { Button } from '@/components/ui/button';
 import {
