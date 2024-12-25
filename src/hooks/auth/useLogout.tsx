e7c0235ed31ed84cbda1e1720a26c280bed8014e
Safwaan Chowdhury <57375371+Safewaan@users.noreply.github.com>
2024-12-25 13:44:11 -0500

    x (#33)

--- src/hooks/auth/useLogout.tsx
+++ src/hooks/auth/useLogout.tsx
@@ -2,7 +2,7 @@ import { useMutation } from '@tanstack/react-query';
 
 import { client } from '@/hooks/client';
 
-const API_URL = '/sessions/invalidate';
+const API_URL = '/auth/logout';
 
 const invalidateSession = async () => {
   const response = await client.post(API_URL, {}, { withCredentials: true });
