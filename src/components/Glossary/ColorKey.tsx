09d745b3e5c07545f8f9bd79fe6113e02ffda767
raf-c <130236858+raf-c@users.noreply.github.com>
2025-01-19 16:42:48 -0500

    Color Key (#41)
    * :)
    
    * update name
    
    * format

---
+++ src/components/Glossary/ColorKey.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+interface GlossaryRowColorProps {
+  color: string;
+}
+
+export const GlossaryRowColor: React.FC<GlossaryRowColorProps> = ({
+  color,
+}) => {
+  return (
+    <div
+      style={{
+        width: '8px',
+        height: '8px',
+        borderRadius: '50%',
+        backgroundColor: color,
+      }}
+    />
+  );
+};
