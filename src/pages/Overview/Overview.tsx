a316c63f92fba9e7a881c56b1d0ee14c23594374
Raf <130236858+raf-c@users.noreply.github.com>
2025-01-22 20:54:34 -0500

    clean overview

--- src/pages/Overview/Overview.tsx
+++ src/pages/Overview/Overview.tsx
@@ -18,61 +18,6 @@ export const Overview: React.FC = () => {
   return (
     <div>
       <Typography variant="h1">Hello Mr {user?.firstName}</Typography>
-      <div className="flex flex-row">
-        <Card className="w-[872px] h-[510px] m-4"></Card>
-        <Card className="w-[560px] h-[510px] m-4 p-4 pt-16">
-          <div className="flex flex-row gap-[40px] p-[32px]">
-            <Glossary>
-              <GlossaryBody>
-                <GlossaryRow>
-                  <GlossaryRowColor color={'#14B8A6'} />
-                  <GlossaryLabel text="Housing" />
-                  <GlossaryValue text="X%" />
-                </GlossaryRow>
-                <GlossaryRow>
-                  <GlossaryRowColor color={'#F4A261'} />
-                  <GlossaryLabel text="Debt & Fees" />
-                  <GlossaryValue text="X%" />
-                </GlossaryRow>
-                <GlossaryRow>
-                  <GlossaryRowColor color={'#E35D6A'} />
-                  <GlossaryLabel text="Food" />
-                  <GlossaryValue text="X%" />
-                </GlossaryRow>
-                <GlossaryRow>
-                  <GlossaryRowColor color={'#3B82F6'} />
-                  <GlossaryLabel text="Shopping" />
-                  <GlossaryValue text="X%" />
-                </GlossaryRow>
-              </GlossaryBody>
-            </Glossary>
-            <Glossary>
-              <GlossaryBody>
-                <GlossaryRow>
-                  <GlossaryRowColor color={'#6366F1'} />
-                  <GlossaryLabel text="Entertainment" />
-                  <GlossaryValue text="X%" />
-                </GlossaryRow>
-                <GlossaryRow>
-                  <GlossaryRowColor color={'#EC4899'} />
-                  <GlossaryLabel text="Transport & Travel" />
-                  <GlossaryValue text="X%" />
-                </GlossaryRow>
-                <GlossaryRow>
-                  <GlossaryRowColor color={'#F59E0B'} />
-                  <GlossaryLabel text="Health & Wellness" />
-                  <GlossaryValue text="X%" />
-                </GlossaryRow>
-                <GlossaryRow>
-                  <GlossaryRowColor color={'#FACC15'} />
-                  <GlossaryLabel text="Other" />
-                  <GlossaryValue text="X%" />
-                </GlossaryRow>
-              </GlossaryBody>
-            </Glossary>
-          </div>
-        </Card>
-      </div>
     </div>
   );
 };
