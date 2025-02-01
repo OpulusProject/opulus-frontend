7d2ca4e1d69481dcf9022db9f2e7924d29ca74a9
raf-c <130236858+raf-c@users.noreply.github.com>
2025-02-01 16:12:50 -0500

    Raf/LineChart (#50)
    * x
    
    * x
    
    * Fix label and clean story
    
    * lint

---
+++ src/components/LineChart/LineChart.stories.tsx
@@ -0,0 +1,45 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { LineChart } from '@/components/LineChart/LineChart';
+import { Card } from '@/components/ui/card';
+
+const meta: Meta<typeof LineChart> = {
+  title: 'Components/LineChart',
+  component: LineChart,
+};
+
+export default meta;
+type Story = StoryObj<typeof LineChart>;
+
+const data = [
+  { date: new Date(2023, 0, 1), value: 500 },
+  { date: new Date(2023, 1, 1), value: 400 },
+  { date: new Date(2023, 2, 1), value: 300 },
+  { date: new Date(2023, 3, 1), value: 600 },
+  { date: new Date(2023, 4, 1), value: 700 },
+  { date: new Date(2023, 5, 1), value: 800 },
+  { date: new Date(2023, 6, 1), value: 950 },
+  { date: new Date(2023, 7, 1), value: 850 },
+  { date: new Date(2023, 8, 1), value: 1000 },
+  { date: new Date(2023, 9, 1), value: 950 },
+  { date: new Date(2023, 10, 1), value: 900 },
+  { date: new Date(2023, 11, 1), value: 700 },
+  { date: new Date(2024, 0, 1), value: 500 },
+  { date: new Date(2024, 1, 1), value: 600 },
+  { date: new Date(2024, 2, 1), value: 700 },
+  { date: new Date(2024, 3, 1), value: 800 },
+  { date: new Date(2024, 4, 1), value: 900 },
+  { date: new Date(2024, 5, 1), value: 1000 },
+  { date: new Date(2024, 6, 1), value: 1100 },
+  { date: new Date(2024, 7, 1), value: 1400 },
+  { date: new Date(2024, 8, 1), value: 1300 },
+  { date: new Date(2024, 9, 1), value: 1200 },
+];
+
+export const Default: Story = {
+  render: () => (
+    <Card className="w-[530px] h-[530px]">
+      <LineChart data={data} dataKey="value" label="Net Worth" />
+    </Card>
+  ),
+};
---
+++ src/components/LineChart/LineChart.tsx
@@ -0,0 +1,163 @@
+import React from 'react';
+import {
+  CartesianGrid,
+  LabelList,
+  Line,
+  LineChart as LineGraph,
+  XAxis,
+} from 'recharts';
+
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from '@/components/ui/chart';
+
+interface LineChartData {
+  date: Date; // Use Date object for the date field
+  [key: string]: number;
+}
+
+interface LineChartProps {
+  data: LineChartData[];
+  dataKey: string;
+  label: string;
+  color?: string;
+  showXAxis?: boolean;
+  showGridLines?: boolean;
+}
+
+export const LineChart: React.FC<LineChartProps> = ({
+  data,
+  dataKey,
+  label,
+  color = '#00FFAF',
+  showXAxis = false,
+  showGridLines = false,
+}) => {
+  const chartConfig: ChartConfig = {
+    [dataKey]: {
+      label,
+      color,
+    },
+  };
+
+  // Find the highest and lowest Y-values
+  const maxData = Math.max(...data.map((d) => d[dataKey]));
+  const minData = Math.min(...data.map((d) => d[dataKey]));
+
+  // Find the index of the most recent occurrence of the max and min data values
+  const maxIndex = data
+    .slice()
+    .reverse()
+    .findIndex((d) => d[dataKey] === maxData);
+  const minIndex = data
+    .slice()
+    .reverse()
+    .findIndex((d) => d[dataKey] === minData);
+
+  // Get the corresponding data for max and min (most recent)
+  const maxDataPoint = data[data.length - 1 - maxIndex];
+  const minDataPoint = data[data.length - 1 - minIndex];
+
+  return (
+    <ChartContainer config={chartConfig} className="w-full h-full">
+      <LineGraph
+        accessibilityLayer
+        data={data}
+        margin={{
+          top: 200,
+        }}
+      >
+        {showGridLines && <CartesianGrid vertical={false} />}
+        {showXAxis && (
+          <XAxis
+            dataKey="date"
+            tickLine={false}
+            axisLine={false}
+            tickMargin={8}
+            tickFormatter={(date) => {
+              // Assuming value is a Date object
+
+              // Extract month abbreviation (e.g., "Jan", "Feb")
+              if (date instanceof Date) {
+                return date.toLocaleString('en-US', { month: 'short' });
+              }
+              return '';
+            }}
+          />
+        )}
+        <ChartTooltip
+          cursor={false}
+          content={<ChartTooltipContent />}
+          hideIndicator={true}
+        />
+        <Line
+          dataKey={dataKey}
+          type="linear"
+          stroke={color}
+          strokeWidth={2}
+          dot={false}
+        >
+          {/* Label the highest point (most recent occurrence) */}
+          <LabelList
+            dataKey={dataKey}
+            position="top"
+            content={({
+              x,
+              y,
+              value,
+              index,
+            }: {
+              x: number;
+              y: number;
+              value: number;
+              index: number;
+            }) =>
+              value === maxData && data[index].date === maxDataPoint.date ? (
+                <text
+                  x={x}
+                  y={y - 10}
+                  textAnchor="middle"
+                  fill={'#808080'}
+                  fontSize={12}
+                >
+                  {`${maxData}`}
+                </text>
+              ) : null
+            }
+          />
+          {/* Label the lowest point (most recent occurrence) */}
+          <LabelList
+            dataKey={dataKey}
+            position="bottom"
+            content={({
+              x,
+              y,
+              value,
+              index,
+            }: {
+              x: number;
+              y: number;
+              value: number;
+              index: number;
+            }) =>
+              value === minData && data[index].date === minDataPoint.date ? (
+                <text
+                  x={x}
+                  y={y + 20}
+                  textAnchor="middle"
+                  fill={'#808080'}
+                  fontSize={12}
+                >
+                  {`${minData}`}
+                </text>
+              ) : null
+            }
+          />
+        </Line>
+      </LineGraph>
+    </ChartContainer>
+  );
+};
