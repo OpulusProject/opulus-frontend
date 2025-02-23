b12e0db3c78ff0859499969a4b7757fce6463629
Safwaan Chowdhury <57375371+Safewaan@users.noreply.github.com>
2025-02-23 15:47:46 -0500

    im thinking im back (#61)

---
+++ src/components/custom/InstitutionCard/InstitutionCardBadge.tsx
@@ -0,0 +1,34 @@
+import classNames from 'classnames';
+import { AlertTriangle } from 'lucide-react';
+
+import { Typography } from '@/components/custom/Typography';
+import { Badge } from '@/components/ui/badge';
+
+interface InstitutionCardBadgeProps extends React.PropsWithChildren {
+  className?: string;
+  variant?: 'default' | 'error';
+}
+
+export const InstitutionCardBadge: React.FC<InstitutionCardBadgeProps> = ({
+  className,
+  children,
+  variant = 'default',
+}) => {
+  // default color is a guess
+  const textColor =
+    variant === 'default' ? 'text-popover-foreground' : 'text-destructive';
+
+  return (
+    <Badge
+      variant="secondary"
+      className={classNames(
+        `pointer-events-none flex flex-row rounded-lg mt-4 gap-2 secondary ${textColor}`,
+        className,
+        'InstitutionCardBadge-root'
+      )}
+    >
+      {variant === 'error' && <AlertTriangle size={16} />}
+      <Typography variant="l2">{children}</Typography>
+    </Badge>
+  );
+};
